99da9c5102620a848696d4972172b76e70ebe869
dev-ethar <tinayousofiali@gmail.com>
2022-10-21 09:31:10 -0700

    input component done now onto searching functions

--- mongoose/front-end/components/Inputs.tsx
+++ mongoose/front-end/components/Inputs.tsx
@@ -36,7 +36,7 @@ const Inputs : NextPage = () => {
             </div>
             <input type="text" id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="phone" />
         </div>
-        <button type="submit" className="text-white bg-green-700 p-1 py-2 md:py-1 md:px-5 md:text-[1rem] text-2xl px-3 rounded-sm border-none outline-none transition-all duration-300 hover:-translate-y-1 w-5/12 md:w-fit">Create</button>
+        <button type="submit" className="text-white bg-green-700 py-1 text-center px-4 md:py-2 md:px-5 md:text-[1rem] text-sm  rounded-sm border-none outline-none transition-all duration-300 hover:-translate-y-1 w-3/2 md:w-fit">Create</button>
     </div>
   )
 }
